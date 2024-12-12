8f07dc3b90058334b4ddf442a0fa8962386bb2dd
Idan17 <idanbartov@gmail.com>
2024-12-12 17:39:18 +0200

    feat: initial layout grid

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -11,10 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        button10.setOnClickListener {
+            reset()
+
         }
+
     }
+
+    // player winning count
+    var player1Count = 0
+    var player2Count = 0
+
 }
