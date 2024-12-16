88c1850c094c55f3e611d5e6d6a9e45db711596c
Idan17 <idanbartov@gmail.com>
2024-12-16 19:40:55 +0200

    feat: initial layout

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,26 +1,13 @@
 package com.example.assignment1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        button10.setOnClickListener {
-            reset()
-
-        }
-
     }
-
-    // player winning count
-    var player1Count = 0
-    var player2Count = 0
-
 }
