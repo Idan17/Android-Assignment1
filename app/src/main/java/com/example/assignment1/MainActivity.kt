8c4291cc355038ec7868d6ee40fa774dd51eafd1
Idan17 <idanbartov@gmail.com>
2024-12-24 16:22:22 +0200

    fix: comments in mainActivity

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.assignment1
-
 import android.os.Bundle
 import android.widget.Button
-//import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.assignment1.R
 import com.example.assignment1.R.*
 
 class MainActivity : AppCompatActivity() {
